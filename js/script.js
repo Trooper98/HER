9ac39a7cd487f9d1e5092758acd6cbd5cd4c8de8
Hanien <hanienkobus@gmail.com>
2018-05-05 23:58:46 +0200

    upon entering the date of your surgery you get to a page which displays the last date you are allowed to eat. Shower and brushing teeth dates will be implemented soon.

--- js/script.js
+++ js/script.js
@@ -1,28 +1,48 @@
-var monthtext = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
+var monthtext = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 
+                'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 var dayField;
 var monthField;
 var yearField;
 var surgeryDate;
+
 
 function getDateSurgery() {
     dayField = document.getElementById("day").value;
     monthField = document.getElementById("month").value;
     yearField = document.getElementById("year").value;
 
-    console.log(dayField);
-    console.log(monthField);
-    console.log(yearField);
+    console.log("day:" + dayField);
+    console.log("month:" + monthField);
+    console.log("year:" + yearField);
 
     surgeryDate = new Date(yearField, monthField, dayField);
+    if(typeof(Storage) != "undefined"){
+        localStorage.setItem("surgeryDay", dayField);
+        localStorage.setItem("surgeryMonth", monthField);
+        localStorage.setItem("surgeryYear", yearField);
+        console.log("Success");
+    } else {
+        console.log("No storage");
+    }
 }
 
 function calculateFood(){
-    var foodDate = surgeryDate.getDate();
+    console.log("year:" + localStorage.getItem("surgeryYear"));
+
+    var foodYear = localStorage.getItem("surgeryYear");
+    var foodMonth = localStorage.getItem("surgeryMonth");
+    var foodDay = localStorage.getItem("surgeryDay") - 1;
+    /*var foodDate = new Date(localStorage.getItem("surgeryYear"), 
+        localStorage.getItem("surgeryMonth"), 
+        localStorage.getItem("surgeryDay")); */
+    var foodDate = new Date(foodYear, foodMonth, foodDay);
     
-    foodDate.setDate(surgeryDate - 1);
+    console.log("foodDate day: " + foodDate.getDate());
     // fetches paragraph element for foodDate
-    var foodDateEl = document.getElementById(row3).firstChild();
-    foodDateEl.innerHTML = foodDate.toString();
+    var foodDateEl = document.getElementById("foodDate");
+    foodDateEl.innerHTML = foodDate.getDate() + "-"
+                            + monthtext[foodDate.getMonth()]
+                            + "-" + foodDate.getUTCFullYear();
 }
 
 function calculateShower() {
