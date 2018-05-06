a2c5753b772961235e87519b34eab86692e63691
Trooper98 <oliver.g.ryan@gmail.com>
2018-05-06 10:20:02 +0200

    cleaning up dashboard

--- js/script.js
+++ js/script.js
@@ -121,3 +121,40 @@ function calculateTeeth() {
     var daysLeftTeethEl = document.getElementById("daysLeftTeeth");
     daysLeftTeethEl.innerHTML = daysLeft;
 }
+
+function countdown(){
+
+    var surgeryYear = localStorage.getItem("surgeryYear");
+    var surgeryMonth = localStorage.getItem("surgeryMonth") - 1;
+    var surgeryDay = localStorage.getItem("surgeryDay") - 1;
+    
+    var surgeryDate = new Date(surgeryYear, surgeryMonth, 
+                            surgeryDay);
+        // Set the date we're counting down to
+        var countDownDate = surgeryDate;
+
+        // Update the count down every 1 second
+        var x = setInterval(function () {
+
+            // Get todays date and time
+            var now = new Date().getTime();
+
+            // Find the distance between now an the count down date
+            var distance = countDownDate - now;
+
+            // Time calculations for days, hours, minutes and seconds
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // Display the result in the element with id="gameday"
+            document.getElementById("gameDay").innerHTML = days + " days and " + hours + " hours";
+
+            // If the count down is finished, write some text
+            if (distance < 0) {
+                clearInterval(x);
+                document.getElementById("gameDay").innerHTML = "EXPIRED";
+            }
+        }, 1000);
+}
